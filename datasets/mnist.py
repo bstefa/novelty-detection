1c6cb1634a37f5ba21de833cb3fd1c24e31a243c
Alexis Pascual <apascua2@uwo.ca>
2021-02-26 11:50:51 -0500

    Adding toy MNIST datamodule

---
+++ datasets/mnist.py
@@ -0,0 +1,66 @@
+import os
+import pytorch_lightning as pl
+from typing import Optional, Generic
+from torch.utils.data import random_split, DataLoader
+from torchvision import datasets, transforms
+
+class MNISTDataModule(pl.LightningDataModule):
+
+    def __init__(
+            self,
+            transforms: Optional[transforms.Compose] = None
+        ):
+
+        super(MNISTDataModule, self).__init__()
+
+        self.download_dir = "./downloads"
+        self.batch_size = 32
+        self.transforms = transforms.Compose([
+
+            transforms.ToTensor()
+
+        ])
+
+        if os.path.isdir(download_dir) == False:
+            print("Creating download directory")
+            os.makedirs(download_dir)
+
+    def prepare_data(self):
+
+        datasets.MNIST(self.download_dir,
+            train=True,
+            download=True)
+
+        datasets.MNIST(self.download_dir,
+            train=False,
+            download=True)
+
+    def setup(self, stage=Optional[str]=None):
+
+        data = datasets.MNIST(self.download_dir,
+                              train=True,
+                              transform=self.transforms)
+        
+        self.train_data, self.val_data = random_split(data, [55000, 5000])
+  
+        self.test_data = datasets.MNIST(self.download_dir, 
+                                        train = False, 
+                                        transform = self.transform)
+
+    def train_dataloader(self):
+        return DataLoader(
+            self.train_data,
+            batch_size=self.batch_size,
+            num_workers=4)
+
+    def val_dataloader(self):
+        return DataLoader(
+            self.val_data,
+            batch_size=self.batch_size,
+            num_workers=4)
+
+    def test_dataloader(self):
+        return DataLoader(
+            self.test_data,
+            batch_size=self.batch_size,
+            num_workers=4z)
--- experiments/trainer_cvae.py
+++ experiments/trainer_cvae.py
@@ -1,10 +1,20 @@
 import pytorch_lightning as pl
 from models.cvae import VariationalAutoEncoder
+from dataset.mnist import MNISTDataModule
+from modules.cvae_base_module import CVAEBaseModule
 
 def main():
 	# Set defaults
     DEFAULT_CONFIG_FILE = 'configs/trainer_cae_reference.yaml'
     config = tools.config_from_command_line(DEFAULT_CONFIG_FILE)
+
+    datamodule = MNISTDataModule()
+
+    model = VariationalAutoEncoder(1, 10, [6, 12, 24], 28, 28)
+
+    module = CVAEBaseModule(datamodule, model, config)
+
+    #TODO: figure out configs
 
 if __name__ == '__main__':
 	main()
