d3c526fc7cace7e903e2d8a0e510157ca1e02ad6
Braden Stefanuk <bstefanuk@gmail.com>
2021-03-04 13:33:09 -0500

    Training PCA model; still need to address forgotten data samples; performance appears to be quite poor at this time

--- experiments/pca.py
+++ experiments/pca.py
@@ -1,11 +1,13 @@
 """
-Script used to run novelty detection experiment using Principal
-Component Analysis.
+Script used to run novelty detection experiment using incremental Principal
+Component Analysis. This script trains and serializes the model for future
+evaluation.
 
 Uses:
     Module: PCABaseModule
     Model: IncrementalPCA
     Dataset: LunarAnalogueDataGenerator
+    Configuration: incremental_pca.yaml
 """
 import pickle
 
@@ -16,24 +18,27 @@ from modules.pca_base_module import PCABaseModule
 
 
 def main():
-    default_config_file = 'configs/pca.yaml'
+    default_config_file = 'configs/incremental_pca.yaml'
+    # Optionally accepts configuration file specified on command line
     config = tools.config_from_command_line(default_config_file)
 
     # Initialize datagenerator
-    datagenerator = LunarAnalogueDataGenerator(config)
+    datagenerator = LunarAnalogueDataGenerator(config, stage='train')
 
-    # Initialize model. With n_component=None the number of features will
-    # autoscale to the batch size
+    # Initialize model. With n_component=None the number of features will autoscale to the batch size
     model = IncrementalPCA(n_components=None)
 
     # Initialize experimental module
     module = PCABaseModule(datagenerator, model, config)
 
     # Incrementally fit training set with PCA
-    datagenerator.setup('train')
-    module.fit_pca(fast_dev_run=1)
+    validation_novelty_scores = module.fit_pipeline()
 
-    with open('models/saved-models/IncrementalPCA-LunarAnalogue-WholeImage.p', 'wb') as f:
+    # Do some serializing
+    with open('logs/IncrementalPCA/LunarAnalogue-WholeImage-ValidationNoveltyScores.p', 'wb') as f:
+        pickle.dump(validation_novelty_scores, f)
+
+    with open('models/saved-models/IncrementalPCA/LunarAnalogue-WholeImage.p', 'wb') as f:
         pickle.dump(module.model, f)
 
 
--- modules/pca_base_module.py
+++ modules/pca_base_module.py
@@ -1,16 +1,15 @@
+"""
+Defines base module for PCA experiments.
+Class must be instantiated with an sklearn model that has the following methods: .partial_fit, .transform,
+.inverse_transform
+"""
 import numpy as np
-import matplotlib.pyplot as plt
 
 from functools import reduce
-from sklearn import model_selection
-from utils import tools, losses
+from utils import losses
 
 
 class PCABaseModule(object):
-    '''
-    Base module for PCA-based experiments
-    '''
-
     def __init__(
             self,
             datagenerator,
@@ -21,81 +20,72 @@ class PCABaseModule(object):
         self.model = model
         self.hparams = params
 
-    def fit_pca(self, fast_dev_run: int=0):
-        '''
-        Function. Uses the class's datagenerator to fit the PCA model on the
-        entire dataset.
-        Returns self
-        '''
-        # TODO: Consider doing cross validation (say, 10-fold) on each batch
+    def fit_pipeline(self, fast_dev_run: int=0):
+        """
+        Uses the class's training and validation generator to iteratively fit/validate the PCA model.
+        """
+        novelty_scores = []
         for batch_nb, batch_tr_in in enumerate(self.dg.trainval_generator('train')):
             print(f'[BATCH {batch_nb}] Fitting...')
             self.model.partial_fit(self._flatten_batch(batch_tr_in))
 
-            running_novelty_score = 0
+            batch_novelty_scores = []
             for batch_vl_nb, batch_vl_in in enumerate(self.dg.trainval_generator('val')):
+
                 batch_vl_rd = self.model.transform(self._flatten_batch(batch_vl_in))
                 batch_vl_rc = self.model.inverse_transform(batch_vl_rd).reshape(batch_vl_in.shape)
 
                 # Run validation on batch
-                for x_hat, x in zip(batch_vl_rc, batch_vl_in):
-                    assert len(x.shape) == 3 and len(x_hat.shape), 'Only accepts H,W,C image-data'
-                    running_novelty_score += losses.squared_error(
-                        x,
-                        x_hat,
-                        show_plot=(True if batch_nb % 10 == 9 else False),
+                for x_nb, (x_rc, x_in) in enumerate(zip(batch_vl_rc, batch_vl_in)):
+                    assert len(x_rc.shape) == 3 and len(x_hat.shape), 'Only accepts H,W,C image-data'
+                    image_novelty_score = losses.squared_error(
+                        x_in,
+                        x_rc,
+                        show_plot=(True if all([batch_nb % 10 == 0, x_nb == 4]) else False),
                         return_map=False
                     )
-            print(f'[BATCH {batch_nb}] Validation novelty score {running_novelty_score/(len(batch_vl_in)*batch_vl_nb)}')
+                    batch_novelty_scores.append(image_novelty_score)
+
+            print(f'[BATCH {batch_nb}] Validation novelty score {np.mean(batch_novelty_scores)}')
+            novelty_scores.append(batch_novelty_scores)
 
             if fast_dev_run > 0 and batch_nb == (fast_dev_run - 1):
-                return self
                 break
-        return self
+        return novelty_scores
 
     def transform_pipeline(self, fast_dev_run: int=0):
+        """
+        Uses the class's test generator to iteratively transform and evaluate novelty scores for the
+        trained PCA model.
+        """
         novelty_scores = []
-        for batch_nb, (batch_rd, batch_in, batch_label) in enumerate(self.transform_generator()):
-            print('Transform: ', batch_nb)
-            # Compute batch-wise reconstruction
-            batch_rc = self.inverse_transform(batch_rd).reshape(batch_in.shape)
-            # Operate on each image-pair independently
-            for x_nb, (x_hat, x) in enumerate(zip(batch_rc, batch_in)):
-                assert len(x.shape) == 3, 'Can only operate on H,W,C image-data'
+        novelty_labels = []
+        for batch_nb, (batch_in, batch_lbl) in enumerate(self.dg.test_generator()):
+
+            # Compute batch-wise reduction and reconstruction
+            batch_rd = self.model.transform(self._flatten_batch(batch_in))
+            batch_rc = self.model.inverse_transform(batch_rd).reshape(batch_in.shape)
+
+            # Operate on each image-pair independently, looping over the batch
+            for x_nb, (x_rc, x_in, x_lbl) in enumerate(zip(batch_rc, batch_in, batch_lbl)):
+                assert len(x_rc.shape) == 3, 'Can only operate on H,W,C image-data'
                 # TODO: Consider using alternative losses here
-                image_novelty_score = losses.squared_error(
-                    x,
-                    x_hat,
+                x_novelty_score = losses.squared_error(
+                    x_in,
+                    x_rc,
                     show_plot=(True if all([batch_nb % 10 == 0, x_nb == 0]) else False),
                     return_map=False
                 )
-                novelty_scores.append(image_novelty_score)
+                novelty_scores.append(x_novelty_score)
+                novelty_labels.append(x_lbl)
+
+            print(f'[BATCH {batch_nb}] Transforming...')
 
             if fast_dev_run > 0 and batch_nb == (fast_dev_run - 1):
                 break
 
         # TODO: This is an ideal place to return a BatchStatistics object in the future
-        return np.array(novelty_scores)
-
-    def transform_generator(self):
-        """
-        Generator. Generates data and transforms it with a trained PCA model
-        on the fly. Useful for iterating through the results and working with the transformations
-
-        Returns a tuple generator (reduced data--flattened, input data--image shape)
-        that can ce called as an iterable.
-        """
-        for batch_in, batch_label in self.dg.test_generator():
-            yield self.model.transform(self._flatten_batch(batch_in)), batch_in, batch_label
-
-    def inverse_transform(self, reduced_batch_in):
-        '''
-        Function.
-        reduced_batch_in: Reduced data. Available after running .transform method on
-        sklearn estimator. For this wrapper, it's used to convert a single batch
-        back to the original data space in conjunction with the transform generator.
-        '''
-        return self.model.inverse_transform(reduced_batch_in)
+        return novelty_scores, novelty_labels
 
     @property
     def components(self):
