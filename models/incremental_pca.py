a73562a0410a8e7e25bcf8cd86920f53e3c7df6a
brahste <b.stefanuk@shaw.ca>
2021-03-04 14:44:48 -0500

    Clean up PCA module and model, remove some unused code

--- models/incremental_pca.py
+++ models/incremental_pca.py
@@ -2,32 +2,34 @@ from sklearn import decomposition
 
 class IncrementalPCA():
     def __init__(self, n_components: int):
-        self.ipca = decomposition.IncrementalPCA(n_components)
-        return
+        self._ipca = decomposition.IncrementalPCA(n_components)
 
     def partial_fit(self, batch_in):
-        self.ipca.partial_fit(batch_in)
+        self._ipca.partial_fit(batch_in)
 
     def transform(self, batch_in):
-        return self.ipca.transform(batch_in)
+        return self._ipca.transform(batch_in)
 
     def inverse_transform(self, batch_in):
-        return self.ipca.inverse_transform(batch_in)
+        return self._ipca.inverse_transform(batch_in)
 
     @property
     def components_(self):
-        return self.ipca.components_
+        return self._ipca.components_
 
     @property
     def explained_variance_(self):
-        return self.ipca.explained_variance_
+        return self._ipca.explained_variance_
 
     @property
     def explained_variance_ratio_(self):
-        return self.ipca.explained_variance_ratio_
-
+        return self._ipca.explained_variance_ratio_
 
     @property
-    def meanvar_(self):
-        return self.ipca.mean_, self.ipca.var_
+    def mean_(self):
+        return self._ipca.mean_
+    
+    @property
+    def var_(self):
+        return self._ipca.var_
 
