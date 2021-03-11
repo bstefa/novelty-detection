5ce9097d2838c26f18fc62f7cc7f866f6937e41b
Braden Stefanuk <bstefanuk@gmail.com>
2021-03-11 14:24:21 -0500

    CAE design complete and super clean, see experiment notebook for some basic results

--- modules/cae_base_module.py
+++ modules/cae_base_module.py
@@ -1,173 +1,65 @@
-import os
 import torch
 import torch.nn as nn
 import pytorch_lightning as pl
-import torchvision
-
-from utils.tools import *
 
 
 class CAEBaseModule(pl.LightningModule):
-    """
-    The lighting module helps enforce best practices
-    by keeping your code modular and abstracting the
-    'engineering code' or boilerplate that new model
-    require.
-    """
-
     def __init__(
             self,
-            datamodule,
-            model,
-            params: dict
+            model: nn.Module,
+            learning_rate: float = 0.001,
+            weight_decay_coefficient: float = 0.01,
     ):
         super(CAEBaseModule, self).__init__()
 
-        # Use datamodule and model
-        self.dm = datamodule
         self.model = model
-
-        # Set hparams (anything set as an hparam is automatically saved)
-        self.hparams = params
+        self.lr = learning_rate
+        self.wd = weight_decay_coefficient if weight_decay_coefficient is not None else 0.01
 
         # Return a callable torch.nn.XLoss object
-        # e.g. self._loss_function = self._handle_loss_function(params['loss_function'])
+        # TODO: Add the ability to quickly change the loss function being implemented (e.g. SSIM)
         self.loss_function = nn.MSELoss()
 
     def forward(self, x):
         return self.model.forward(x)
 
-    def train_dataloader(self):
-        return self.dm.train_dataloader()
-
-    def val_dataloader(self):
-        return self.dm.val_dataloader()
-
-    def test_dataloader(self):
-        return self.dm.test_dataloader()
-
     def configure_optimizers(self):
-        return torch.optim.Adam(
-            self.model.parameters(),
-            lr=self.hparams.learning_rate
-        )
+        return torch.optim.AdamW(self.model.parameters(), lr=self.lr, weight_decay=self.wd)
 
-    def on_train_start(self):
-        print(f'Initializing with parameters:\n{self.hparams}\n')
+    def training_step(self, batch_in, batch_nb):
 
-    def on_epoch_start(self):
-        random_integers = torch.randint(
-            low=0,
-            high=self.hparams.num_train_batches,
-            size=(3,)
-        )
-        self._random_train_steps = self.global_step + random_integers
-        print(f'\nLogging images from batches: {self._random_train_steps.tolist()}\n')
+        batch_rc = self(batch_in)
+        loss = self.loss_function(batch_rc, batch_in)
 
-    def training_step(self, x_in, batch_nb):
+        self.log('train_loss', loss)
+        return {'loss': loss}  # The returned object must contain a 'loss' key
 
-        x_rc = self(x_in)
-        loss = self.loss_function(x_rc, x_in)
+    def validation_step(self, batch_in, batch_nb):
 
-        images = {
-            'x_in': x_in.detach(),  # Tensor
-            'x_rc': x_rc.detach()  # Tensor
-        }
-        result = {
-            'loss': loss
-        }
+        batch_rc = self(batch_in)
+        loss = self.loss_function(batch_rc, batch_in)
 
-        # Log some data
-        if any([x == self.global_step for x in self._random_train_steps]):
-            self._handle_image_logging(images, session='train')
+        self.log('val_loss', loss)
+        return {'val_loss': loss}
 
-        self.log_dict(result)
-        return result  # The returned object must contain a 'loss' key
+    def test_step(self, batch_tuple, batch_nb):
 
-    def validation_step(self, x_in, batch_nb):
+        batch_in, batch_labels = batch_tuple
+        batch_rc = self(batch_in)
+        loss = self.loss_function(batch_rc, batch_in)
 
-        x_rc = self(x_in)
-        loss = self.loss_function(x_rc, x_in)
+        # Calculate individual novelty scores
+        batch_scores = torch.empty(len(batch_labels), dtype=torch.float)
+        for x_nb, (x_rc, x_in) in enumerate(zip(batch_rc, batch_in)):
+            batch_scores[x_nb] = self.loss_function(x_rc, x_in)
 
-        result = {
-            'val_loss': loss
-        }
-
-        self.log_dict(result)
-        return result
-
-    def test_step(self, x_in, batch_nb):
-
-        x_rc = self(x_in)
-        loss = self.loss_function(x_rc)
-
-        print(loss)
-
-    def _handle_image_logging(self, images: dict, session: str = 'train'):
-
-        if self.logger.version is None:
-            return
-        else:
-            compute = {
-                'x_in_01': unstandardize_batch(images['x_in']),
-                'x_rc_01': unstandardize_batch(images['x_rc']),
-                'error_map': get_error_map(images['x_in'], images['x_rc'])
+        self.log('test_loss', loss)
+        return {
+            'test_loss': loss,
+            'scores': batch_scores,
+            'labels': batch_labels,
+            'images': {
+                'batch_in': batch_in.detach(),  # Tensor
+                'batch_rc': batch_rc.detach()  # Tensor
             }
-
-            self._log_to_tensorboard(images, compute)
-            self._log_images(compute)
-        return
-
-    def _log_to_tensorboard(self, result: dict, compute: dict):
-        self.logger.experiment.add_image(
-            f'x_in-{self.global_step}',
-            result['x_in'],
-            global_step=self.global_step,
-            dataformats='NCHW'
-        )
-        self.logger.experiment.add_image(
-            f'x_in_unstandardized-{self.global_step}',
-            compute['x_in_01'],
-            global_step=self.global_step,
-            dataformats='NCHW'
-        )
-        self.logger.experiment.add_image(
-            f'x_rc-{self.global_step}',
-            result['x_rc'],
-            global_step=self.global_step,
-            dataformats='NCHW'
-        )
-        self.logger.experiment.add_image(
-            f'x_rc_unstandardized-{self.global_step}',
-            compute['x_rc_01'],
-            global_step=self.global_step,
-            dataformats='NCHW'
-        )
-        self.logger.experiment.add_image(
-            f'metric_squared_error-{self.global_step}',
-            compute['error_map'],
-            global_step=self.global_step,
-            dataformats='NCHW'
-        )
-
-    def _log_images(self, compute: dict):
-
-        logger_save_path = os.path.join(
-            self.logger.save_dir,
-            self.logger.name,
-            f'version_{self.logger.version}'
-        )
-
-        if not os.path.exists(os.path.join(logger_save_path, 'images')):
-            os.mkdir(os.path.join(logger_save_path, 'images'))
-
-        rint = torch.randint(0, len(compute['x_in_01']), size=())
-        for key in compute:
-            torchvision.utils.save_image(
-                compute[key][rint],
-                os.path.join(
-                    logger_save_path,
-                    'images',
-                    f'{self.current_epoch}-{self.global_step - (self.hparams.num_train_batches * self.current_epoch)}-{key}.png'
-                )
-            )
+        }
