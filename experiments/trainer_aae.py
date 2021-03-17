a921cc5f509bbdaaca46b91eec09efc18c945b91
brahste <b.stefanuk@shaw.ca>
2021-03-17 17:20:38 -0400

    Add logger, visualization callback, and validation to simple AAE

--- experiments/trainer_aae.py
+++ experiments/trainer_aae.py
@@ -7,59 +7,67 @@ import pytorch_lightning as pl
 from models.simple_aae import SimpleAAE
 from datasets import supported_datamodules
 from modules.aae_base_module import AAEBaseModule
-from utils import tools
+from utils import tools, callbacks
 from torchsummary import summary
 from functools import reduce
 
 
-DEFAULT_CONFIG_FILE = 'configs/aae/aae_emnist.yaml'
+def main():
+    # Load configs
+    print('[INFO] Loading configs...')
+    config = tools.config_from_command_line(DEFAULT_CONFIG_FILE)
+    # Unpack configuration
+    exp_params = config['experiment-parameters']
+    data_params = config['data-parameters']
+    module_params = config['module-parameters']
+
+    # Initialize datamodule
+    print('[INFO] Initializing datamodule..')
+    datamodule = supported_datamodules[data_params['datamodule']](**data_params)
+    datamodule.prepare_data()
+    datamodule.setup('train')
+
+    # Initialize model
+    print('[INFO] Initializing model..')
+    model = SimpleAAE(reduce(lambda x, y: x*y, datamodule.shape), **module_params)
+
+    # Initialize module
+    print('[INFO] Initializing module..')
+    module = AAEBaseModule(
+        model,
+        train_size=datamodule.train_size,
+        val_size=datamodule.val_size,
+        batch_size=datamodule.batch_size,
+        **module_params)
+
+    # Initialize loggers to monitor training and validation
+    print('[INFO] Initializing logger..')
+    logger = pl.loggers.TestTubeLogger(
+        exp_params['log_dir'],
+        name=exp_params['name'])
+
+    # Initialize the Trainer object
+    print('[INFO] Initializing trainer..')
+    trainer = pl.Trainer(
+        weights_summary=None,
+        gpus=1,
+        logger=logger,
+        max_epochs=100,
+        check_val_every_n_epoch=1,
+        callbacks=[
+            pl.callbacks.early_stopping.EarlyStopping(monitor='val_r_loss', patience=5),
+            pl.callbacks.ModelCheckpoint(monitor='val_r_loss', filename='{val_r_loss:.2f}-{epoch}', save_last=True),
+            callbacks.AAEVisualization()
+        ])
+
+    # Train the model
+    print('[INFO] Training model...')
+    trainer.fit(module, datamodule)
+
+    # Some final saving once training is complete
+    tools.save_object_to_version(config, version=module.version, filename='configuration.yaml', **exp_params)
 
 
-# Load configs
-print('[INFO] Loading configs...')
-config = tools.config_from_command_line(DEFAULT_CONFIG_FILE)
-# Unpack configuration
-exp_params = config['experiment-parameters']
-data_params = config['data-parameters']
-module_params = config['module-parameters']
-
-# Initialize datamodule
-print('[INFO] Initializing datamodule..')
-datamodule = supported_datamodules[exp_params['datamodule']](**data_params)
-datamodule.prepare_data()
-datamodule.setup('train')
-
-print(reduce(lambda x, y: x*y, datamodule.shape))
-print(datamodule.shape)
-
-# Initialize model
-print('[INFO] Initializing model..')
-model = SimpleAAE(reduce(lambda x, y: x*y, datamodule.shape), **module_params)
-
-# View a summary of the model
-# summary(model.to(torch.device('cuda' if torch.cuda.is_available() else 'cpu')), datamodule.shape)
-
-# Initialize module
-print('[INFO] Initializing module..')
-module = AAEBaseModule(
-    model,
-    train_size=datamodule.train_size,
-    val_size=datamodule.val_size,
-    batch_size=datamodule.batch_size,
-    **module_params)
-
-# Initialize the Trainer object
-print('[INFO] Initializing trainer..')
-trainer = pl.Trainer(
-    weights_summary=None,
-    gpus=1,
-    max_epochs=100,
-    check_val_every_n_epoch=1,
-    callbacks=[
-        pl.callbacks.early_stopping.EarlyStopping(monitor='loss', patience=5)
-    ])
-
-# Train the model
-print('[INFO] Training model...')
-trainer.fit(module, datamodule)
-
+if __name__ == '__main__':
+    DEFAULT_CONFIG_FILE = 'configs/aae/aae_emnist.yaml'
+    main()
--- models/simple_aae.py
+++ models/simple_aae.py
@@ -11,10 +11,10 @@ class Encoder(nn.Module):
         # TODO: Consider initializing the weights explicitly here
         self.encoder = nn.Sequential(
             nn.Linear(in_chans, 1000),
-            nn.Dropout(p=0.2),
+            nn.Dropout(p=0.3),
             nn.ReLU(),
             nn.Linear(1000, 1000),
-            nn.Dropout(p=0.2),
+            nn.Dropout(p=0.3),
             nn.ReLU(),
             nn.Linear(1000, latent_chans))
 
@@ -49,10 +49,10 @@ class Discriminator(nn.Module):
         # TODO: Consider initializing the weights explicitly here
         self.discriminator = nn.Sequential(
             nn.Linear(latent_chans, 1000),
-            nn.Dropout(p=0.1),
+            nn.Dropout(p=0.2),
             nn.ReLU(),
             nn.Linear(1000, 1000),
-            nn.Dropout(p=0.1),
+            nn.Dropout(p=0.2),
             nn.ReLU(),
             nn.Linear(1000, 1),
             nn.Sigmoid())
@@ -73,26 +73,33 @@ class SimpleAAE(nn.Module):
     def prior(self, *size):
         return torch.randn(*size, device=self.device)
 
-    def generator_loss(self, batch_in: Tensor):
+    def generator_loss(self, batch_in: Tensor, epsilon: float = 1e-8):
         self.encoder.train()
         self.discriminator.eval()
 
         z_generated = self.encoder(batch_in)
-        return -torch.mean(torch.log(self.discriminator(z_generated)))
+        d_score = self.discriminator(z_generated)
 
-    def discriminator_loss(self, batch_in: Tensor):
+        assert not any(torch.isnan(d_score)), 'Can\'t pass NaN to logarithm'
+        assert not any(d_score < 0.), 'Can\'t pass negative number to logarithm'
+
+        return -torch.mean(torch.log(d_score + epsilon))
+
+    def discriminator_loss(self, batch_in: Tensor, epsilon: float = 1e-8):
         self.discriminator.train()
         self.encoder.eval()  # Freeze dropout and other non-training parameters
 
         z_fake = self.encoder(batch_in)
-        d_loss_fake = self.discriminator(z_fake)
+        d_score_fake = self.discriminator(z_fake)
 
         # The discriminator take samples with size of the latent space
         z_real = self.prior(*z_fake.shape)
-        d_loss_real = self.discriminator(z_real)
+        d_score_real = self.discriminator(z_real)
 
-        # TODO: Refamiliarize yourself with the concept of log-likelihood
-        return -torch.mean(torch.log(d_loss_real) + torch.log(1 - d_loss_fake))
+        assert not any(d_score_fake < 0.), 'Abort. Can\'t pass negative number to logarithm'
+        assert not any(d_score_real < 0.), 'Abort. Can\'t pass negative number to logarithm'
+        # TODO: Replace this loss with losses already implement in Pytorch is possible
+        return -torch.mean(torch.log(d_score_real + epsilon) + torch.log(1 - d_score_fake + epsilon))
 
     def reconstruction_loss(self, batch_in: Tensor):
         self.encoder.train()
