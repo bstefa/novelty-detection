04f6743712e8b9d55b8a5db5cb21551b9bb2e17c
Braden Stefanuk <bstefanuk@gmail.com>
2021-04-19 11:46:00 -0400

    Move visualizations to callback

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,11 @@
 from .aae_simple import SimpleAAE
+from .vae_simple import SimpleVAE
 from .cae_baseline import BaselineCAE
 from .cae_compression import CompressionCAEMidCapacity, CompressionCAEHighCapacity
 
 supported_models = {
     'SimpleAAE': SimpleAAE,
+    'SimpleVAE': SimpleVAE,
     'BaselineCAE': BaselineCAE,
     'CompressionCAEMidCapacity': CompressionCAEMidCapacity,
     'CompressionCAEHighCapacity': CompressionCAEHighCapacity
