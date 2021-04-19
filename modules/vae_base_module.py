2e98b9b687a2b7500bf52c6a6627b7adad9d3d26
Braden Stefanuk <bstefanuk@gmail.com>
2021-04-19 13:48:33 -0400

    Adapt VAE architecture to match other models a bit better

--- modules/vae_base_module.py
+++ modules/vae_base_module.py
@@ -27,45 +27,38 @@ class VAEBaseModule(pl.LightningModule):
     def configure_optimizers(self):
         return torch.optim.Adam(self.model.parameters(), lr=self.lr)
 
-    def training_step(self, batch, batch_idx, optimizer_idx=0):
+    def training_step(self, batch, batch_idx):
 
-        images, labels = batch
-        # Results is a list of outputs computed during the forward pass
-        # of the form: [reconstructions, input, mu, log_var]
-        results_list = self.forward(images, labels=labels)
+        batch_in, _ = batch
+        batch_rc, mu, log_var = self.forward(batch_in)
 
-        # Note that train_loss is a dictionary containing the various losses
-        train_loss_dict = self.model.loss_function(
-            *results_list,
-            M_N=self._batch_size / self._train_size,
-            optimizer_idx=optimizer_idx,
-            batch_idx=batch_idx)
+        elbo_loss, rc_loss, kld_loss = self.model.loss_function(
+            batch_rc, batch_in, mu, log_var, M_N=self._batch_size / self._train_size)
 
-        self.log_dict(train_loss_dict)
-        return train_loss_dict
+        result = {'loss': elbo_loss, 'rc_loss': rc_loss, 'kld_loss': kld_loss}
+        self.log_dict(result)
+        return result  # Needs 'loss' key in return dict
 
-    def validation_step(self, batch, batch_idx, optimizer_idx=0):
-        real_img, labels = batch
+    def validation_step(self, batch, batch_idx):
+        batch_in, _ = batch
 
-        results_list = self.forward(real_img, labels=labels)
-        val_loss_dict = self.model.loss_function(
-            *results_list,
-            M_N=self._batch_size / self._val_size,
-            optimizer_idx=optimizer_idx,
-            batch_idx=batch_idx)
+        batch_rc, mu, log_var = self.forward(batch_in)
 
-        return val_loss_dict
+        elbo_loss, rc_loss, kld_loss = self.model.loss_function(
+            batch_rc, batch_in, mu, log_var, M_N=self._batch_size / self._train_size)
+
+        result = {'val_elbo_loss': elbo_loss, 'val_rc_loss': rc_loss, 'val_kld_loss': kld_loss}
+        self.log_dict(result, on_epoch=True)
+        return result
 
     def validation_epoch_end(self, outputs):
-        avg_elbo_loss = torch.stack([x['loss'] for x in outputs]).mean()
-        avg_recons_loss = torch.stack([x['reconstruction_loss'] for x in outputs]).mean()
-        avg_kld_loss = torch.stack([x['KLD'] for x in outputs]).mean()
-
+        # TODO: This may be unneeded after specifying 'on_epoch=True' in validation step
+        avg_elbo_loss = torch.stack([x['val_elbo_loss'] for x in outputs]).mean()
+        avg_recons_loss = torch.stack([x['val_rc_loss'] for x in outputs]).mean()
+        avg_kld_loss = torch.stack([x['val_kld_loss'] for x in outputs]).mean()
         self.log('avg_elbo_loss', avg_elbo_loss)
         self.log('avg_recons_loss', avg_recons_loss)
         self.log('avg_kld_loss', avg_kld_loss)
-
-        pass
 
     def test_step(self, batch, batch_nb):
         batch_in, batch_labels = batch
@@ -75,12 +68,11 @@ class VAEBaseModule(pl.LightningModule):
         recons_error = mse_loss(recons, batch_in)
         mse_loss_sum = torch.sum(recons_error, dim=(1, 2, 3))
 
-        results_dict = {
+        results = {
             'scores': mse_loss_sum,
             'labels': batch_labels
         }
-
-        return results_dict
+        return results
 
     @property
     def version(self):
